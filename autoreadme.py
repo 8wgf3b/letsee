77ae8dc2371d1fe6261cdb849671eddfc5bb5168
8wgf3b <karnabalaj@gmail.com>
2020-11-11 14:49:30 +0530

    Clean repo

--- autoreadme.py
+++ autoreadme.py
@@ -1,6 +1,6 @@
 import glob
 
-count = len(glob.glob('[0-9+].q'))
+count = len(glob.glob('q/[0-9+].q'))
 
 readme = "# Project Euler Solutions in kdb-q\n\n"
 
@@ -11,8 +11,8 @@ with open('names.txt', 'r') as f:
 
 for i in range(1, count + 1):
     readme += f'[{names[i - 1].rstrip()}](https://projecteuler.net/problem={i}) | '
-    readme += f'[{i}.q]({i}.q) | '
-    readme += f'[input](inputs/{i}.txt)\n'
+    readme += f'[{i}.q](q/{i}.q) | '
+    readme += f'[input](q/inputs/{i}.txt)\n'
 
 with open('README.md', 'w') as f:
     f.write(readme)
